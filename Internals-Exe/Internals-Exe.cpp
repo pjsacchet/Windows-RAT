2846a5158375ab03714e256ae685f9ebaf33915c
Patrick Sacchet <patrick.j.sacchet@gmail.com>
2022-01-24 21:56:57 -0500

    Adding base debug files for cpp (copy); testing loadlibrary functionality for DLL operations

--- Internals-Exe/Internals-Exe.cpp
+++ Internals-Exe/Internals-Exe.cpp
@@ -2,6 +2,7 @@
 // Version 1.0
 // Internals-Exe.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
+#include <windows.h>
 #include <iostream>
 
 
@@ -14,7 +15,15 @@ int main()
 
     // If we are in a 'operational' space load our DLL into memory so it can call back to our client 
     // TODO: Dll load call for Internals-DLL
+    HINSTANCE hGetProcIDLL = LoadLibrary(L"C:\\WInternals-DLL.dll");
 
+    if (!hGetProcIDLL)
+    {
+        std::cout << "Could not successfully load target DLL \n";
+        return -1;
+    }
+
+    std::cout << " Successfully loaded target DLL!\n";
 
     // While our DLL handles commands from client continue to survey the system, calling DLL_PROCESS_DETACH once we suspect a threat
     // TODO: Implement further antivirus monitoring 
